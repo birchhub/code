c5357757b3b1f7578aef096e05195f4c1514ffa8
leo hoeckner <lhoeckner@barracuda.com>
2020-07-30 14:26:12 +0200

    if no range is configured, do not throw

--- network/httpserver.py
+++ network/httpserver.py
@@ -21,12 +21,15 @@ def get_free_ips(ipRange):
     conn=open_db(DBPATH)
     ips=[]
     if (conn):
-        cur=conn.cursor()
-        cur.execute(f"select ip from 'range{ipRange}' where status=0 order by lastonline desc")
+        try:
+            cur=conn.cursor()
+            cur.execute(f"select ip from 'range{ipRange}' where status=0 order by lastonline asc")
 
-        rows=cur.fetchall()
-        for row in rows:
-            ips.append(row[0])
+            rows=cur.fetchall()
+            for row in rows:
+                ips.append(row[0])
+        except:
+            return []
 
     return ips
 
@@ -35,33 +38,36 @@ def netdiscover_overview(ipRange):
     conn=open_db(DBPATH)
     result=[]
     if (conn):
-        cur=conn.cursor()
-        cur.execute(f"select ip,status,lastonline,range.mac,m1.domain,"
-                    f"range.dup1,m2.domain,range.dup2,m3.domain "
-                    f"from 'range{ipRange}' as range "
-                    f"left join macDomainMap as m1 on range.mac = m1.mac " 
-                    f"left join macDomainMap as m2 on (range.dup1 = m2.mac) "
-                    f"left join macDomainMap as m3 on (range.dup2 = m3.mac)")
+        try:
+            cur=conn.cursor()
+            cur.execute(f"select ip,status,lastonline,range.mac,m1.domain,"
+                        f"range.dup1,m2.domain,range.dup2,m3.domain "
+                        f"from 'range{ipRange}' as range "
+                        f"left join macDomainMap as m1 on range.mac = m1.mac " 
+                        f"left join macDomainMap as m2 on (range.dup1 = m2.mac) "
+                        f"left join macDomainMap as m3 on (range.dup2 = m3.mac)")
 
-        rows=cur.fetchall()
-        for row in rows:
-            mac=row[3]
-            domainname=row[4]
-            if not domainname:
-                domainname = ""
-            dup1=row[5]
-            if dup1 and dup1 != "na":
-                mac += f" {dup1}"
-                domainname += f" {row[6]}"
-                dup2=row[7]
-                if dup2 and dup2 != "na":
-                    mac += f" {dup2}"
-                    domainname += f" {row[8]}"
+            rows=cur.fetchall()
+            for row in rows:
+                mac=row[3]
+                domainname=row[4]
+                if not domainname:
+                    domainname = ""
+                dup1=row[5]
+                if dup1 and dup1 != "na":
+                    mac += f" {dup1}"
+                    domainname += f" {row[6]}"
+                    dup2=row[7]
+                    if dup2 and dup2 != "na":
+                        mac += f" {dup2}"
+                        domainname += f" {row[8]}"
 
-            next_ip = {"ip":row[0], "status":row[1], "lastOnline":row[2], "mac":mac, "domainname":domainname}
-            result.append(next_ip)
+                next_ip = {"ip":row[0], "status":row[1], "lastOnline":row[2], "mac":mac, "domainname":domainname}
+                result.append(next_ip)
+        except:
+            return json.dumps([])
 
-    return json.dumps(result )
+    return json.dumps(result)
 
 def addMacDomainMapping(mac, domain):
     # curl --data '{"mac":"52:54:00:c1:58:6b", "domain":"kurtsMasterDomain"}' localhost:3141
@@ -81,6 +87,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
         self.data_string = self.rfile.read(int(self.headers['Content-Length']))
         self.send_response(200)
         self.end_headers()
+        print(self.data_string)
         data = json.loads(self.data_string)
         
         mac=data["mac"]
